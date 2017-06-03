d695585276286ce63ed4a9c35da1911bee2d31ea
Mihir Patil <mihir.patil@berkeley.edu>
2017-06-03 01:31:36 -0700

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,6 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='negative-cycles',
 
@@ -25,7 +19,7 @@ setup(
     version='0.1.0',
 
     description='For Negative Cycle Detection',
-    long_description=long_description,
+    long_description='',
 
     # The project's main homepage.
     url='https://github.com/mnpatil17/negative_cycles/',

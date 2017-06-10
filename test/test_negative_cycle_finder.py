97bd1c89204767c14d51a25c8efd8ff523adca1e
Mihir Patil <mihir.patil@berkeley.edu>
2017-06-10 01:32:35 -0700

    Minor testing changes

--- test/test_negative_cycle_finder.py
+++ test/test_negative_cycle_finder.py
@@ -19,8 +19,8 @@ class TestNegativeCycles(unittest.TestCase):
 
     def test_small_graph(self):
 
-        currency_list = np.array(['A', 'B', 'C'])
-        graph = np.eye(len(currency_list))
+        node_labels = np.array(['A', 'B', 'C'])
+        graph = np.eye(len(node_labels))
 
         # A to B and back
         graph[0][1] = 1 / 1.5
@@ -34,14 +34,14 @@ class TestNegativeCycles(unittest.TestCase):
         graph[1][2] = 16.0
         graph[2][1] = 1 / 16.0
 
-        cycle, gain = find_negative_cycle(graph, currency_list)
+        cycle, gain = find_negative_cycle(graph, node_labels)
         self._check_array(cycle, ['A', 'B', 'C', 'A'])
         self._check(gain, 1.0666666666666667)
 
     def test_four_node_graph(self):
 
-        currency_list = np.array(['A', 'B', 'C', 'D'])
-        graph = np.eye(len(currency_list))
+        node_labels = np.array(['A', 'B', 'C', 'D'])
+        graph = np.eye(len(node_labels))
 
         # A to B and back
         graph[0][1] = 1 / 1.09
@@ -67,18 +67,18 @@ class TestNegativeCycles(unittest.TestCase):
         graph[2][3] = 10.191
         graph[3][2] = 1 / 10.191
 
-        cycle, gain = find_negative_cycle(graph, currency_list)
+        cycle, gain = find_negative_cycle(graph, node_labels)
         self._check_none(cycle)
         self._check_none(gain)
 
     def test_live_example_graph(self):
 
-        currency_list = np.array(['A', 'B', 'C', 'D'])
+        node_labels = np.array(['A', 'B', 'C', 'D'])
         graph = np.array([[1.00000000e+00, 1.00000000e+00, 1.09110747e-02, 4.21229992e-02],
                           [1.00000000e+00, 1.00000000e+00, 1.87899286e+01, 7.40192450e+01],
                           [9.16500000e+01, 5.32200000e-02, 1.00000000e+00, None],
                           [2.37400000e+01, 1.35100000e-02, None, 1.00000000e+00]])
 
-        cycle, gain = find_negative_cycle(graph, currency_list)
+        cycle, gain = find_negative_cycle(graph, node_labels)
         self._check_array(cycle, ['B', 'C', 'A', 'B'])
         self._check(gain, 1722.0969)
